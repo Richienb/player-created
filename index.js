01ff25dabaf0bdcd2c48a62c22c9c5ed275f33a7
Richie Bendall <richiebendall@gmail.com>
2020-08-09 17:25:12 +1200

    Detect when it is impossible to find the creation date
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 "use strict"
 
-const ky = require("ky-universal")
-const pDoWhilst = require("p-do-whilst")
-const numberDifference = require("num-diff")
 const minecraftPlayer = require("minecraft-player")
 const mem = require("mem")
-
-async function checkUsernameAtTime(username, timestamp) {
-	const { status } = await ky(username, {
-		prefixUrl: "https://api.mojang.com/users/profiles/minecraft/",
-		searchParams: {
-			at: timestamp
-		}
-	})
-
-	return status === 200
-}
-
-const startTime = 1263146630
-const endTime = Math.floor(Date.now() / 1000)
 
 module.exports = mem(async username => {
 	if (typeof username !== "string") {
@@ -27,25 +10,13 @@ module.exports = mem(async username => {
 
 	const { createdAt } = await minecraftPlayer(username)
 
-	if (createdAt !== null) {
-		return new Date(createdAt)
+	if (createdAt === null) {
+		throw new Error("This account was created before 01/10/2010 @ 6:03pm (UTC), had 2 or more usernames assigned to it or is unmigrated.")
 	}
 
-	let previousGuess
-	let currentGuess = startTime + Math.floor(numberDifference(endTime, startTime) / 2)
+	if (createdAt === undefined) {
+		throw new Error("This account has had its username changed at least once")
+	}
 
-	await pDoWhilst(
-		async () => {
-			const beforeGuess = previousGuess
-			previousGuess = currentGuess
-			if (await checkUsernameAtTime(username, currentGuess)) {
-				currentGuess -= Math.floor(numberDifference(currentGuess, beforeGuess || startTime) / 2)
-			} else {
-				currentGuess += Math.floor(numberDifference(currentGuess, beforeGuess || endTime) / 2)
-			}
-		},
-		() => previousGuess !== currentGuess
-	)
-
-	return new Date(currentGuess * 1000)
+	return new Date(createdAt)
 })
--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 const test = require("ava")
-const inRange = require("in-range")
 const playerCreated = require(".")
 
 test("main", async t => {
 	t.is((await playerCreated("Richienb")).getTime(), 1546732800000)
-	t.true(inRange((await playerCreated("123lmfao4")).getTime(), {
-		start: 1263146640000,
-		end: 1263146650000
-	})) // Legacy account
+	await t.throwsAsync(() => playerCreated("123lmfao4"))
 })
